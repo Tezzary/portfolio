3701dd986b97500adfb8e1edef7cf47e9275ac9a
tezzy <zacharyeverett.00@gmail.com>
2025-03-28 00:08:55 +1100

    some basic styling, linking of pages, adding of images, basic transition on hover of project, not much work on other pages yet

---
+++ index.js
@@ -0,0 +1,7 @@
+
+document.getElementById('golfzio').addEventListener('click', function() {
+    location.href = 'golfzio.html';
+});
+document.getElementById('rlposter').addEventListener('click', function() {
+    location.href = 'rlposter.html';
+});
